bd6e381a162b1fcf9ecd76c2f0b6e81a58f319d3
Mohammad Khatib <mkhatib727@gmail.com>
2015-06-04 20:30:58 -0700

    Initial push for project structure and main activity with a ListView.

---
+++ app/src/main/java/me/magicalunicorn/android/gloomy/app/ForecastFragment.java
@@ -0,0 +1,56 @@
+package me.magicalunicorn.android.gloomy.app;
+
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/65",
+                "Thurs - Astreoids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP Trapped in Wheatherstation - 60/51",
+                "Sun - Sunny - 80/68"
+        };
+        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(forecastAdapter);
+
+        return rootView;
+    }
+}
